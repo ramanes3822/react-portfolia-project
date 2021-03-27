bda73b5c97cb058541463954a223c4e11adaf0d0
ramanes <ramanes3822@gmail.com>
2021-03-27 18:41:38 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./component/Navbar";
+import Header from "./component/Header";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        params={{
+          particles: {
+            number: { value: 30, density: { enable: true, value_area: 900 } },
+            shape: { type: "square", stroke: { width: 6, color: "#f9ab00" } },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
