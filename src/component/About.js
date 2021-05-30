69707a7d1ca085defc46f14340ef13832ac397d1
ramanes <ramanes3822@gmail.com>
2021-05-30 18:34:50 +0800

    about me changes

--- src/component/About.js
+++ src/component/About.js
@@ -13,20 +13,11 @@ const About = () => {
         <div className="col-lg-6 col-xm-12">
           <h1 className="about-heading">ABOUT ME</h1>
           <p>
-            Hello! I am Ramanes. I have been developing websites for over 5
-            years. I'm Full-Stack Web Developer. Technologies I use is
-            MERN(MongoDB, Express, ReactJS and NodeJS). I create responsive
-            websites that are displayed on all devices desktops and smartphones.
-            Of course, before I begin developing any webapp, Landing Page,
-            Business Website or E-commerce, I need to have a ready-made project
-            layout (sketch). And I’m ready to do this for you, before we start
-            developing your website, we will discuss all the details of your
-            niche with you, I will conduct a survey of your competitors and make
-            a list of their advantages and weaknesses. All this is necessary to
-            give your customers something that your competitors do not provide,
-            or to present it too, but better! After the website is finished and
-            ready to deploy and sell a product or service. It is essential that
-            your potential clients know about you.
+            Hello! I am Ramanes. I have been developing websites for over 3
+            years. Technologies I use is MERN(MongoDB, Express, ReactJS and
+            NodeJS). I create responsive websites that are displayed on all
+            devices desktops and smartphones.I develop this website to show my
+            ability on creating React based website.
           </p>
         </div>
       </div>
